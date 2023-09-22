880b6f0a9b998001373667d8b6465678c22cea26
Salomaofl <100690807+Salomaofl@users.noreply.github.com>
2023-09-21 21:11:02 -0300

    Metodo buscar no nó

--- src/main/java/com/mycompany/lista/ListaEncadeada.java
+++ src/main/java/com/mycompany/lista/ListaEncadeada.java
@@ -4,19 +4,19 @@ package com.mycompany.lista;
  *
  * @author salomao.flima
  */
-public class ListaEncadeada <Tipo>{
+public class ListaEncadeada<Tipo> {
+
     //Atributos da classe
-    private No inicio; 
-    private No fim; 
+    private No inicio;
+    private No fim;
     private int tamanho;
-    
+
     //Metodo Construtor
     public ListaEncadeada() {
         this.tamanho = 0;
     }
-    
-    //Aqui temos os metodos de acesso
 
+    //Aqui temos os metodos de acesso
     public No getInicio() {
         return inicio;
     }
@@ -41,23 +41,32 @@ public class ListaEncadeada <Tipo>{
         this.tamanho = tamanho;
     }
 // Metodo para exibir as informações
+
     @Override
     public String toString() {
         return "ListaEncadeada{" + "inicio=" + inicio + '}';
     }
-    
+
     public void adiciona(Tipo elemento) {
-        No <Tipo> celula = new No <Tipo> (elemento);
-        if (this.inicio == null && this.fim == null){
-        this.inicio = celula;
-        this.fim = celula;
-       } else {
+        No<Tipo> celula = new No<Tipo>(elemento);
+        if (this.inicio == null && this.fim == null) {
+            this.inicio = celula;
+            this.fim = celula;
+        } else {
             this.fim.setProximo(celula);
             this.fim = celula;
+        }
+        this.tamanho++;
+
+    }
+
+    public No get(int posicao) {
+        No atual = this.inicio;
+        for (int i = 0; i < posicao; i++) {
+            if (atual.getProximo() != null) {
+                atual = atual.getProximo();
             }
-    this.tamanho++;
-    
+        }
+        return atual;
+    }
 }
-}
-
-
